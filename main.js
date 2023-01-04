c6f0af03500dd4d66f46fac153d377b0f367ea49
mattWStevens <gibsonhistoric@gmail.com>
2023-01-03 20:50:15 -0700

    Implementing main logic and animations for the experience section.

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 let currExperienceItem = 1;
 
 function navigateExperience(direction) {
+    // Update experience item.
     const navValue = direction === 'back' ? -1 : 1;
     currExperienceItem += navValue;
+
     if (currExperienceItem === 0) { currExperienceItem = 3; }
     if (currExperienceItem === 4) { currExperienceItem = 1; }
-    
+
+    const experienceContainerCurrent = document.getElementsByClassName(`experience__item--${currExperienceItem}`)[0];
+    const githubLink = document.getElementsByClassName('link-button')[0];
     const navNumber = document.getElementById('experienceNavNumber');
-    navNumber.textContent = `${currExperienceItem}/3`;
+
+    // Apply animations.
+    experienceContainerCurrent.style.animation = 'change 2s linear';
+    experienceContainerCurrent.firstElementChild.style.animation = 'fadeIn 2s';
+    if (githubLink) { githubLink.style.animation = 'fadeInLink 2s'; }
+
+    // Keep updating of experience item number display in sync with ripple animation.
+    setTimeout(() => {
+        navNumber.textContent = `${currExperienceItem}/3`;
+    }, 200);
+
+    // Update display.
     const experienceItems = document.getElementsByClassName('experience__item');
 
     for (let i = 0; i < experienceItems.length; i++) {
         if (i + 1 !== currExperienceItem) {
-            experienceItems[i].style.visibility = 'hidden';
+            experienceItems[i].style.display = 'none';
         } else {
-            experienceItems[i].style.visibility = 'visible';
+            experienceItems[i].style.display = 'block';
         }
     }
 }
